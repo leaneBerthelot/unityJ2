c4b140867e2ea290d5c7b5920ff6103b92733805
lucie <non@nope.fr>
2024-03-05 16:05:09 +0100

    le fun

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    private GameObject gameState;
+    void Start() {
+        gameState = GameObject.Find("GameState");
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.tag == "Collectible") {
+            return;
+        }
+        
+        int height = Random.Range(0, 3);
+        int score = gameState.GetComponent<Utils>().IncrementScore();
+
+        if (score % 5 == 0) {
+            gameState.GetComponent<Utils>().IncrementSpeed();
+        }
+        Instantiate(GameObject.FindGameObjectsWithTag("Collectible")[0], new Vector3(25, height + 2f, 10), Quaternion.identity);
+        Instantiate(other.GameObject(), new Vector3(17, height, 10), Quaternion.identity);
+    }
+}
